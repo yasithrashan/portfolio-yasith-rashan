a58e004ff3c005fdc14b94f1e4552ad11ccf7985
Yasith Rashan <y.rashan22@gmail.com>
2025-06-10 02:07:41 +0530

    Added metadata icons to page.tsx and made minor formatting adjustments

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -3,7 +3,12 @@ export const metadata = {
   description: "Welcome to my personal portfolio website.",
   keywords: ["Yasith Rashan", "Yasith", "Portfolio"],
   authors: [{ name: "Yasith Rashan" }],
+  icons: {
+    icon: "/logo.svg",
+  },
 };
+
+
 
 import AboutMe from "./components/AboutMe";
 import Achievements from "./components/Achievements";

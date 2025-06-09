be44131e7ba2a0ce0649370d05de0f5ca16bded5
Yasith Rashan <y.rashan22@gmail.com>
2025-06-09 20:41:18 +0530

    feat: added HeroSection component and updated globals.css and page.tsx files

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,17 +1,10 @@
-import Image from "next/image";
+
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="mt-4 text-lg">This is a simple Next.js application.</p>
-      <Image
-        src="/nextjs-logo.png"
-        alt="Next.js Logo"
-        width={200}
-        height={200}
-        className="mt-8"
-      />
-    </main>
+    <div>
+      <HeroSection />
+    </div>
   );
 }

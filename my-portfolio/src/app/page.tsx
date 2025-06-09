41c887f77b336de2a4d098d40f2e2d29e7e29ac3
Yasith Rashan <y.rashan22@gmail.com>
2025-06-09 21:33:05 +0530

    Added metadata object to page.tsx file

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,3 +1,9 @@
+export const metadata = {
+  title: "Yasith Rashan",
+  description: "Welcome to my personal portfolio website.",
+  keywords: ["Yasith Rashan", "Yasith", "Portfolio"],
+  authors: [{ name: "Yasith Rashan" }],
+};
 
 import HeroSection from "./components/HeroSection";
 
